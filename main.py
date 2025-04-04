29cf64a087086fe2bd1d26a93052276661d8b79b
Gonzalez <agonzalez479@cps.k12.il.us>
2025-04-04 12:00:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("I like pizza!")
+print("It's really good!")
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+is_student = True
+for_sale = False
---
+++ main2.py
@@ -0,0 +1,35 @@
+# what are variables?
+
+# there are differnt types of variables
+# strings are text
+name = "John" # quotation marks
+name2 = "Lucy"
+# integers are whole numbers
+num1 = "10" # no quotation marks
+num2 = 20
+print(int(num1) + num2)
+# a plus sign between two variables its called 
+#concatenation
+# type casting when you convert a 
+# variable from one type to another
+# print(name +"and"+ name2 + "have"+ num1 + str(num2) + "apples")
+# f strings allow us to insert variables into strings
+# using f beofre the string
+print(f"{name} and {name2} have {num1 + num2} apples")
+
+# floats are decimal numbers
+dollars = 10.99
+# it can be postive or negative
+print(f"{name} has {dollars} dollars")
+# booleans are true or false
+is_student = True
+# it can be true or false
+print(f"{name} is a student: {is_student}")
+# lists are collections of values
+# dictionaries are collections of key-value pairs
+# problem set #1 
+# 1. create 4 different types of variables
+
+# 2. print them out
+
+# 3. use f strings to format the strings
