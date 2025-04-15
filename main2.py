8e15c941d608a4b015147b8d964dff17d6c04478
Gonzalez <agonzalez479@cps.k12.il.us>
2025-04-15 11:47:11 -0500

    conditonals

--- main2.py
+++ main2.py
@@ -33,3 +33,60 @@ print(f"{name} is a student: {is_student}")
 # 2. print them out
 
 # 3. use f strings to format the strings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if condionals
+# if statements are used to check if a conditon is True or False
+# if conditon is true, do something
+# if conditon is false, do something else
+
+# if condional statements always start with if
+# and depend on a boolean value  (true or false)
+# example
+classStarted = True
+if classStarted:
+    print("class has started")
+else:
+    print("class has not started")
+
+# logical and comparison operator 
+# == equal to
+# != not equal to 
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+# and logical operators
+# or logical operators
+# not logical operators
+# example
+age = 18
+if age >= 18:
+    print("you are an adult")
+elif age == 17:
+    print("you are almost an adult")
+else:
+    print("you are a minor")
+
+#problem set #2
+# 1. create a program that checks if a number
+# is even or odd
+# 2. ask user for a number
+number = int(input("what is your number"))
+# 3. check if number us even or odd
+if number % 2 == 0:
+    print (f"{number} is even")
+else:
+    print(f"{number} is odd")
